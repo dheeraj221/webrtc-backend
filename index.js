1893d7c9eca88dc93d277023b3176b697a7f5c5e
Dheeraj Patel <dpatel@gammastack.com>
2022-02-21 11:54:34 +0530

    fix: header cors fixing

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 4000;
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => callback(null, true),
+  })
+);
 
 const channels = [
   { name: "test", password: "test" },
@@ -20,9 +25,28 @@ const server = app.listen(PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "*",    
-    methods: ["GET", "POST"],
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
   },
+});
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, content-type, Authorization, Content-Type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
 });
 
 app.get("/active-channels", (req, res) => {
